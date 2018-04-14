d930d0128664a447b9d9e6c90e8629f07c7a5e73
jaderzk <jader.rojas@gmail.com>
2018-04-14 13:06:57 -0500

    Cambio de orden de los condicionales

--- ejerciciosTDD/lib/FizzBuzz.rb
+++ ejerciciosTDD/lib/FizzBuzz.rb
@@ -3,15 +3,21 @@ class FizzBuzz
     $numeros = { 3 =>"Fizz", 5 =>"Buzz"}
 	
 	def es_divisible? numero
-       if numero % 3 ==0
-       	"Fizz"
-       elsif numero % 5 ==0
-       	"Buzz"
-       elsif numero % 3 ==0 and numero % 5 ==0
-        "FizzBuzz"	
-       end
-       
+    if numero % 3 ==0 and numero % 5 ==0  
+          $numeros[3]+$numeros[5]
+    elsif numero % 3==0
+          $numeros[3]          
+    elsif numero % 5==0
+          $numeros[5] 
+    end 
 	end
-     
+end
 
+
+ejemplo = FizzBuzz.new
+
+10.times do
+print "Ingrese número: "
+numero=gets.chomp.to_i
+puts ejemplo.es_divisible? numero
 end
